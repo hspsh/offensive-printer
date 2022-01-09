01aa0759ab18c3380d4653cb5467e0bc1a2c5876
psuwala <oneat@vp.pl>
2022-01-09 23:14:10 +0100

    created offensive printer

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
+#include "RestClient.h"
+#include "Button.h"
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+#define BUTTON_EXTERNAL 5
+
+Button printer(BUTTON_EXTERNAL, INPUT);
+RestClient client = RestClient("banana.at.hsp.net.pl", 8000);
+bool flag = false;
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
+
+  client.begin("eduram", "zarazcipodam");
+
+  printer.onHold([](){
+             flag = true;
+          });
+}
+
+void loop()
+{
+    delay(100);
+    if(flag){
+             int status = client.post("/print/46", "LINE2=XOX&LINE1=OXO&print=Print");
+             Serial.println(status);
+             flag = false;
+    }
+
+}
