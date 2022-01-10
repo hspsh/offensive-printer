42ada98aa3346994b2388acf0efc39559d1be8d0
psuwala <oneat@vp.pl>
2022-01-10 14:51:15 +0100

    ADDED EXPIRATOR

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,11 @@
  */
 #include "Arduino.h"
 #include "Button.h"
+#include "ESPPerfectTime.h"
 #include "RestClient.h"
-#include <time.h>
+#include "TZ.h"
+#include <ctime>
+#include <stdio.h>
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
@@ -17,7 +20,36 @@
 
 Button printer(BUTTON_EXTERNAL, INPUT);
 RestClient client = RestClient("banana.at.hsp.net.pl", 8000);
-bool flag = false;
+uint8_t times = 0;
+
+void updateTime() { pftime::configTime(TZ_Europe_Warsaw, "0.pl.pool.ntp.org"); }
+
+char *expirationDate(unsigned int daysToAdd) {
+  const uint8 SIZE = 11;
+  static char date[SIZE];
+
+  time_t currentTime;
+  time(&currentTime);
+  currentTime += daysToAdd * 24 * 3600;
+
+  struct tm *timeinfo;
+  timeinfo = localtime(&currentTime);
+
+  strftime((char *)&date, SIZE, "%F", timeinfo);
+
+  return (char *)&date;
+}
+
+char *createQueryString(const char *variableName,
+                        const char *escapedStringForQuery) {
+  const uint8 SIZE = 100;
+  static char queryString[SIZE];
+
+  snprintf((char *)queryString, SIZE, "%s=%s&print=Print", variableName,
+           escapedStringForQuery);
+
+  return (char *)&queryString;
+}
 
 void setup() {
   // initialize LED digital pin as an output.
@@ -27,14 +59,16 @@ void setup() {
 
   client.begin("eduram", "zarazcipodam");
 
-  printer.onHold([]() { flag = true; });
+  printer.onHold([]() { times++; });
 }
 
 void loop() {
   delay(100);
-  if (flag) {
-    int status = client.post("/print/46", "LINE2=XOX&LINE1=OXO&print=Print");
+  if (times > 0) {
+    updateTime();
+    int status =
+        client.post("/print/53", createQueryString("EXP", expirationDate(14)));
     Serial.println(status);
-    flag = false;
+    times--;
   }
 }
